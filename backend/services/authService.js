efb040cf9dae04596a36c2f9e05763de541fc8c7
Chayan Nath <53663778+ChayanNath@users.noreply.github.com>
2024-09-21 18:35:40 +0530

    Updated username logic

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -2,11 +2,11 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-// Load environment variables
 const secret = process.env.SECRET_KEY;
 
 exports.register = async (firstName, lastName, username, password) => {
   const hashedPassword = await bcrypt.hash(password, 10);
+  username = username.toLowerCase();
   const user = new User({
     firstName,
     lastName,
@@ -19,19 +19,17 @@ exports.register = async (firstName, lastName, username, password) => {
 
 exports.login = async (username, password) => {
   try {
-    // Find the user by username
+    username = username.toLowerCase();
     const user = await User.findOne({ username });
     if (!user) {
       throw new Error("Invalid username or password");
     }
 
-    // Check if the provided password matches the stored hashed password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       throw new Error("Invalid username or password");
     }
 
-    // Create payload and tokens
     const payload = {
       id: user._id,
       firstName: user.firstName,
@@ -44,14 +42,11 @@ exports.login = async (username, password) => {
       expiresIn: "24h",
     });
 
-    // Update the user's refresh token and save
     user.refreshToken = refreshToken;
-    await user.save(); // Make sure to await the save operation
+    await user.save();
 
-    // Return the payload and tokens
     return { payload, token, refreshToken };
   } catch (error) {
-    // Handle errors
     throw new Error(error.message);
   }
 };
